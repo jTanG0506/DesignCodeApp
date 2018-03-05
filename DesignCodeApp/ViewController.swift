7f52de661c93ae1398f070e189fc4af7434c217a
Jonathan Tang <jonathan.tang@student.manchester.ac.uk>
2018-03-05 12:35:58 +0000

    Create parallax animation with 3D transforms

--- DesignCodeApp/ViewController.swift
+++ DesignCodeApp/ViewController.swift
@@ -68,6 +68,45 @@ extension ViewController: UIScrollViewDelegate {
       deviceImageView.transform = CGAffineTransform(translationX: 0, y: -offsetY / 4)
       backgroundImageView.transform = CGAffineTransform(translationX: 0, y: -offsetY / 3)
     }
+    
+    if let collectionView = scrollView as? UICollectionView {
+      for cell in collectionView.visibleCells as! [SectionCollectionViewCell] {
+        let indexPath = collectionView.indexPath(for: cell)!
+        let attributes = collectionView.layoutAttributesForItem(at: indexPath)!
+        let cellFrame = collectionView.convert(attributes.frame, to: view)
+        
+        // Move the cover image view.
+        let translationX = cellFrame.origin.x / 5
+        cell.coverImageView.transform = CGAffineTransform(translationX: translationX, y: 0)
+        
+        cell.layer.transform = animateCell(cellFrame: cellFrame)
+      }
+    }
+  }
+  
+  func animateCell(cellFrame: CGRect) -> CATransform3D {
+    // 3D perspective view.
+    let angleFromX = Double(-cellFrame.origin.x / 10)
+    let angle = CGFloat(angleFromX * Double.pi / 180)
+    var transform = CATransform3DIdentity
+    transform.m34 = -1.0 / 1000
+    let rotation = CATransform3DRotate(transform, angle, 0, 1, 0)
+    
+    // Scaling.
+    var scaleFromX = (1000 - (cellFrame.origin.x - 200)) / 1000
+    let scaleMax: CGFloat = 1.0
+    let scaleMin: CGFloat = 0.6
+    
+    if scaleFromX > scaleMax {
+      scaleFromX = scaleMax
+    }
+    if scaleFromX < scaleMin {
+      scaleFromX = scaleMin
+    }
+    
+    let scale = CATransform3DScale(CATransform3DIdentity, scaleFromX, scaleFromX, 1)
+    
+    return CATransform3DConcat(rotation, scale)
   }
   
 }
@@ -85,6 +124,8 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     cell.titleLabel.text = section["title"]
     cell.captionLabel.text = section["caption"]
     cell.coverImageView.image = UIImage(named: section["image"]!)
+    
+    cell.layer.transform = animateCell(cellFrame: cell.frame)
     
     return cell
   }
