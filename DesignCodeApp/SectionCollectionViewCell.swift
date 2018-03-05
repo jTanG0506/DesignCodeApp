0b1b7d0b023d2b39ee48448db62b04f3382dc13f
Jonathan Tang <jonathan.tang@student.manchester.ac.uk>
2018-03-05 11:57:21 +0000

    Add data source to Sections on Home screen

---
+++ DesignCodeApp/SectionCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  SectionCollectionViewCell.swift
+//  DesignCodeApp
+//
+//  Created by Jonathan Tang on 05/03/2018.
+//  Copyright © 2018 jtang0506. All rights reserved.
+//
+
+import UIKit
+
+class SectionCollectionViewCell: UICollectionViewCell {
+    
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var captionLabel: UILabel!
+  @IBOutlet weak var coverImageView: UIImageView!
+  
+}
